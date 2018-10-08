5782ca690031a6e30706a9375bc47950abb80288
rz1516 <rz1516@skbl-422-007.ph.ic.ac.uk>
2018-10-08 11:00:48 +0100

    Changed output port to PORT C

--- Simple1.asm
+++ Simple1.asm
@@ -1,18 +1,18 @@
 	#include p18f87k22.inc
 	
 	code
-	org 0x0
-	goto	start
+	org 0x0	
+goto	start
 	
 	org 0x100		    ; Main code starts here at address 0x100
 
 start
 	movlw 	0x0
-	movwf	TRISB, ACCESS	    ; Port C all outputs
+	movwf	TRISC, ACCESS	    ; Port C all outputs
 	bra 	test
-loop	movff 	0x06, PORTB
+loop	movff 	0x06, PORTC
 	incf 	0x06, W, ACCESS
-test	movwf	0x06, ACCESS	    ; Test for end of loop condition
+test	movwf	0x06, ACCESS	    ; Test for end of loop cnodition
 	movlw 	0x63
 	cpfsgt 	0x06, ACCESS
 	bra 	loop		    ; Not yet finished goto start of loop again
