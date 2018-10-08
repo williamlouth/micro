9c01578ef600c0e18b74ecf8f317be9c5ea436de
rz1516 <rz1516@skbl-422-007.ph.ic.ac.uk>
2018-10-08 11:53:21 +0100

    Made it possible to hold down PORTD and set that as the limit

--- Simple1.asm
+++ Simple1.asm
@@ -7,13 +7,17 @@ goto	start
 	org 0x100		    ; Main code starts here at address 0x100
 
 start
-	movlw 	0x0
-	movwf	TRISC, ACCESS	    ; Port C all outputs
+	movwf	TRISC, ACCESS	; Port C all outputs
+	
+	movlw   0xff
+	movwf	TRISD, ACCESS
+	movlw   0x0
+	
 	bra 	test
 loop	movff 	0x06, PORTC
 	incf 	0x06, W, ACCESS
 test	movwf	0x06, ACCESS	    ; Test for end of loop cnodition
-	movlw 	0x63
+	movf	PORTD, W, ACCESS
 	cpfsgt 	0x06, ACCESS
 	bra 	loop		    ; Not yet finished goto start of loop again
 	goto 	0x0		    ; Re-run program from start
