b20514d091874631beddfd77639f80fc5626ee4d
rz1516 <rz1516@skbl-422-007.ph.ic.ac.uk>
2018-10-08 14:54:28 +0100

    Merge origin/master
    Conflicts:
    	Simple1.asm

--- Simple1.asm
+++ Simple1.asm
@@ -6,18 +6,16 @@ goto	start
 	
 	org 0x100		    ; Main code starts here at address 0x100
 
-start
+start	movlw	0x00
 	movwf	TRISC, ACCESS	; Port C all outputs
 	
-	movlw   0xff
-	movwf	TRISD, ACCESS
-	movlw   0x0
+	
 	
 	bra 	test
 loop	movff 	0x06, PORTC
 	incf 	0x06, W, ACCESS
 test	movwf	0x06, ACCESS	    ; Test for end of loop cnodition
-	movf	PORTD, W, ACCESS
+	movlw	0xff
 	cpfsgt 	0x06, ACCESS
 	bra 	loop		    ; Not yet finished goto start of loop again
 	goto 	0x0		    ; Re-run program from start
