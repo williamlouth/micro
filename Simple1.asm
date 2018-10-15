cb74793e0289ca8dcf77c01885837d37f7d565a4
rz1516 <rz1516@skbl-422-007.ph.ic.ac.uk>
2018-10-15 14:42:18 +0100

    Merge origin/master
    Conflicts:
    	Simple1.asm

--- Simple1.asm
+++ Simple1.asm
@@ -1,14 +1,20 @@
 	#include p18f87k22.inc
 	
 	code
+	
 	org 0x0	
 goto	start
+	;banksel PADCFG1 ; PADCFG1 is not in Access Bank!!
 	
 	org 0x100		    ; Main code starts here at address 0x100
 
-start	movlw	0x00
+start	
+	
+	call port_e_pull_up
+	movlw	0x00
 	movwf	TRISD, ACCESS	;Port D all outputs
 	movwf   TRISE, ACCESS   ;Port E all outputs
+	goto	read
 	;movlw   0x2
 	;movwf   PORTD, ACCESS
 	
@@ -33,5 +39,60 @@ to_run  movlw  0x45
 		   
 	bra    to_run
 	goto   0x0		    ; Re-run program from start
+	
+	
+
+port_e_pull_up
+	bsf PADCFG1, REPU, BANKED ; PortE pull-ups on
+	movlb 0x00 ; set BSR back to Bank 0
+	setf TRISE ; Tri-state PortE
+	return
+	
+	
+read	movlw	0x00
+	movwf	TRISD, ACCESS	;Port D all outputs
+	movlw   0xff
+	movwf	TRISE, ACCESS	;Port E all inputs
+	movlw   0x4
+	movwf   PORTD, ACCESS	;set both clocks and 0e's high, stable state
+	
+	movlw  0x0
+	movwf  0x20
+	
+	movlw  0x1
+	movwf  0x21
+	
+	movlw  0x2
+	movwf  0x22
+	
+	movlw  0x3
+	movwf  0x23
+	
+	movlw  0x1
+	
+	cpfseq  0x20
+	bra	0x1
+	bra	read_0
+	
+	cpfseq  0x21
+	bra	0x1
+	bra	read_1
+	
+	return 
+	
+	
+read_0		
+	return 
+	
+read_1
+	return 
+	
+	;return fast
+	
+	
+	
+	
+	
+	
 	
 	end
