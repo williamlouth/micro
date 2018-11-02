2e723eab904136add2a09237dd768a75765fda11
will <williamlouth@outlook.com>
2018-11-02 13:18:30 +0000

    run to 60

---
+++ fft.asm
@@ -0,0 +1,27 @@
+	#include p18f87k22.inc
+	global	fft
+    
+    
+fft_data udata    
+sin res .256
+cos res .256
+	
+fft code
+ 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
